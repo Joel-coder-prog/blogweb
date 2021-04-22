c2c4f54f0eb9710c1944ec87deafd16b7019ab98
Joel-coder-prog <81446210+Joel-coder-prog@users.noreply.github.com>
2021-04-22 16:27:12 -0300

    Add files via upload

--- JS/menu.js
+++ JS/menu.js
@@ -3,3 +3,5 @@ function mostrar_menu(){
     document.getElementById("move-content").classList.toggle('move-container-all');
     document.getElementById("show-menu").classList.toggle('show-lateral');
 }
+
+
